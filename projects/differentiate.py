b5e6b44f1c9eb60b617e76ef14b17151d7daeb14
Kristian Maroni <krisnm@math.uio.no>
2025-08-20 11:18:10 +0200

    Implemented differentiate_vector and fixed error in differentiate.

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -5,13 +5,17 @@ def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
     Nt = len(u) - 1
     d = np.zeros(Nt + 1)
     d[0] = (u[1]-u[0])/dt
-    d[-1] = (u[-1]-u[]-2])/dt
+    d[-1] = (u[-1]-u[-2])/dt
     for n in range(1,Nt):
         d[n] = (u[n+1]-u[n-1])/(2*dt)
     return d
 
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
-    raise NotImplementedError
+    d = np.zeros(len(u))
+    d[0] = (u[1]-u[0])/dt
+    d[-1] = (u[-1]-u[-2])/dt
+    d[1:-1] = (u[2:]-u[0:-2])/(2*dt)
+    return d
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
