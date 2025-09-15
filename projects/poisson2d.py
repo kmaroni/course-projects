bcd4c3a7b76e2dd22e16bad6225fb139ec0b074c
Kristian Maroni <krisnm@math.uio.no>
2025-09-15 16:08:39 +0200

    Implementert Poisson2D løser

--- projects/poisson2d.py
+++ projects/poisson2d.py
@@ -22,15 +22,34 @@ class Poisson2D:
     def create_mesh(self):
         """Return a 2D Cartesian mesh
         """
-        raise NotImplementedError
+        return np.meshgrid(self.px.x, self.py.x, indexing='ij')
 
     def laplace(self):
         """Return a vectorized Laplace operator"""
-        raise NotImplementedError
+        D2x = (1./self.px.dx**2)*self.px.D2()
+        D2y = (1./self.py.dx**2)*self.py.D2()
+        return (sparse.kron(D2x, sparse.eye(self.py.N+1)) +
+                sparse.kron(sparse.eye(self.px.N+1), D2y))
 
-    def assemble(self, f=None):
+    def assemble(self, bc=0, f=None):
         """Return assembled coefficient matrix A and right hand side vector b"""
-        raise NotImplementedError
+        self.xij, self.yij = self.create_mesh()
+        F = sp.lambdify((x,y), f)(self.xij, self.xij)
+        A = self.laplace()
+
+        #Define homogeneous Dirichlet boundary condition
+        B = np.ones((self.px.N,self.py.N), dtype=bool)
+        B[1:-1,1:-1] = 0
+        bnds = np.where(B.ravel() == 1)[0]
+        A = A.tolil()
+        for i in bnds:
+            A[i] = 0
+            A[i, i] = 1
+        A = A.tocsr()
+        b = F.ravel()
+        b[bnds] = bc #equal to constant on boundary
+
+        return A, b
 
     def l2_error(self, u, ue):
         """Return l2-error
@@ -42,9 +61,10 @@ class Poisson2D:
         ue : Sympy function
             The analytical solution
         """
-        raise NotImplementedError
+        uj = sp.lambdify([x, y], ue)(self.px.x,self.py.x)
+        return np.sqrt(self.px.dx*self.py.dx*np.sum((uj-u)**2))
 
-    def __call__(self, f=implemented_function('f', lambda x, y: 2)(x, y)):
+    def __call__(self, bc=0, f=implemented_function('f', lambda x, y: 2)(x, y)):
         """Solve Poisson's equation with a given right hand side function
 
         Parameters
@@ -57,9 +77,16 @@ class Poisson2D:
         The solution as a Numpy array
 
         """
-        A, b = self.assemble(f=f)
+        A, b = self.assemble(bc,f=f)
         return sparse.linalg.spsolve(A, b.ravel()).reshape((self.px.N+1, self.py.N+1))
 
 def test_poisson2d():
     assert False
 
+if __name__ == '__main__':
+    Lx, Ly, Nx, Ny = 1, 1, 100, 100
+    sol = Poisson2D(Lx, Ly, Nx, Ny)
+    ue = x*(1-x)*y*(1-y)
+    u=sol(bc=0,f=ue.diff(x, 2) + ue.diff(y, 2))
+    err = sol.l2_error(u,ue)
+    print(err)
