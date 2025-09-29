d89ece332ca6a789ede1e70f48fc3f15afb7404d
Kristian Maroni <krisnm@math.uio.no>
2025-09-29 13:15:25 +0200

    fikset kode

--- projects/func_approx.py
+++ projects/func_approx.py
@@ -1,15 +1,15 @@
 import numpy as np
 import sympy as sp
 from sympy import besselj
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 
-x = sp.symbols('x') 
+x = sp.symbols('x')
 
 funcs = {#function, interval=(a,b)
-1: (abs(x),(0,1)),
-2: (sp.exp(sp.sin(x)),(0,2)),
-3: (x**10, (0,1),
-4: (sp.exp(-(x-0.5)**2)-sp.exp(-0.25), (0,1)),
-5: (sp.besselj(x),(0,100))
+1:(abs(x),(0,1)),
+2:(sp.exp(sp.sin(x)),(0,2)),
+3:(x**10, (0,1)),
+4:(sp.exp(-(x-0.5)**2)-sp.exp(-0.25), (0,1)),
+5:(sp.besselj(0,x),(0,100))
 }
